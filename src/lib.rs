2f9250c17f236f6baff9ae63829a384ea06c5206
Paul Adenot <paul@paul.cx>
2019-02-25 16:12:05 +0100

    Expose C API

--- src/lib.rs
+++ src/lib.rs
@@ -10,23 +10,54 @@ cfg_if! {
         mod mach_sys;
         extern crate mach;
         extern crate libc;
-        pub use rt_mach::promote_current_thread_to_real_time;
-        pub use rt_mach::demote_current_thread_from_real_time;
+        use rt_mach::promote_current_thread_to_real_time;
+        use rt_mach::demote_current_thread_from_real_time;
         pub use rt_mach::RtPriorityHandle;
     } else if #[cfg(target_os = "windows")] {
         extern crate winapi;
         extern crate kernel32;
         mod rt_win;
-        pub use rt_win::promote_current_thread_to_real_time;
-        pub use rt_win::demote_current_thread_from_real_time;
+        use rt_win::promote_current_thread_to_real_time;
+        use rt_win::demote_current_thread_from_real_time;
         pub use rt_win::RtPriorityHandle;
     } else if #[cfg(target_os = "linux")] {
-        mod rt_linux;
+        pub mod rt_linux;
         extern crate dbus;
         extern crate libc;
-        pub use rt_linux::promote_current_thread_to_real_time;
-        pub use rt_linux::demote_current_thread_from_real_time;
+        use rt_linux::promote_current_thread_to_real_time;
+        use rt_linux::demote_current_thread_from_real_time;
         pub use rt_linux::RtPriorityHandle;
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub struct atp_handle(RtPriorityHandle);
+
+#[no_mangle]
+pub extern "C" fn atp_promote_current_thread_to_real_time(audio_buffer_frames: u32,
+                                           audio_samplerate_hz: u32) -> *mut atp_handle{
+    match promote_current_thread_to_real_time(audio_buffer_frames, audio_samplerate_hz) {
+        Ok(handle) => {
+            Box::into_raw(Box::new(atp_handle(handle)))
+        },
+        _ => {
+            std::ptr::null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn atp_demote_current_thread_from_real_time(handle: *mut atp_handle) -> i32 {
+    assert!(!handle.is_null());
+    let handle = unsafe { Box::from_raw(handle) };
+
+    match demote_current_thread_from_real_time(handle.0) {
+        Ok(_) => {
+            0
+        }
+        _ => {
+            1
+        }
     }
 }
 
