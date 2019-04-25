a09b30e876de10bbed1ea3361321b554ca7766ed
Paul Adenot <paul@paul.cx>
2019-04-25 15:37:36 +0200

    license headers

--- src/rt_mach.rs
+++ src/rt_mach.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use mach::kern_return::kern_return_t;
 use mach::port::mach_port_t;
 use mach_sys::*;
