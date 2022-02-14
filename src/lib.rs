46f626b15cdc3bc3e771e7eae59be79be7858d7e
Paul Adenot <paul@paul.cx>
2022-02-14 11:35:25 +0100

    Add crate-wide doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! # audio_thread_priority
+//!
+//! Promote the current thread, or another thread (possibly in another process), to real-time
+//! priority, suitable for low-latency audio processing.
+//!
+//! # Example
+//!
+//! ```rust
+//!   // ... on a thread that will compute audio and has to be real-time:
+//!   RtPriorityHandle handle;
+//! 
+//!   match promote_current_thread_to_real_time(512, 44100) {
+//!     Ok(h) => {
+//!       handle = h;
+//!       println!("this thread is now bumped to real-time priority.") 
+//!     }
+//!     Err(...) => { println!("could not bump to real time.") }
+//!   }
+//! 
+//!   // do some real-time work.
+//! 
+//!   match demote_current_thread_from_real_time(h) {
+//!     Ok(...) => {
+//!       println!("this thread is now bumped back to normal.")
+//!     }
+//!     Err(...) => {
+//!       println!("Could not bring the thread back to normal priority.")
+//!     }
+//!   }
+//!
+//! ```
 
 #![warn(missing_docs)]
 
