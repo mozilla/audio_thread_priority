6235e7ff5282896a1813500aab15f26a8f27879f
Paul Adenot <paul@paul.cx>
2022-02-14 11:37:33 +0100

    Fix doc formatting

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,17 @@
 //! ```rust
 //!   // ... on a thread that will compute audio and has to be real-time:
 //!   RtPriorityHandle handle;
-//! 
+//!
 //!   match promote_current_thread_to_real_time(512, 44100) {
 //!     Ok(h) => {
 //!       handle = h;
-//!       println!("this thread is now bumped to real-time priority.") 
+//!       println!("this thread is now bumped to real-time priority.")
 //!     }
 //!     Err(...) => { println!("could not bump to real time.") }
 //!   }
-//! 
+//!
 //!   // do some real-time work.
-//! 
+//!
 //!   match demote_current_thread_from_real_time(h) {
 //!     Ok(...) => {
 //!       println!("this thread is now bumped back to normal.")
