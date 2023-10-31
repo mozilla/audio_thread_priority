5be1f393c84ce45865dfadc85d69af6058803086
Matthew Gregan <kinetik@flim.org>
2023-10-31 17:00:46 +1300

    Remove winapi fallback now that windows-sys is widely available.

--- src/rt_win.rs
+++ src/rt_win.rs
@@ -1,39 +1,9 @@
-#[cfg(feature = "windows")]
-mod os {
-    pub use windows::Win32::Foundation::GetLastError;
-    pub use windows::Win32::Foundation::HANDLE;
-    pub use windows::Win32::Foundation::PSTR;
-    pub use windows::Win32::System::Threading::{
-        AvRevertMmThreadCharacteristics, AvSetMmThreadCharacteristicsA,
-    };
-
-    pub fn ok(rv: windows::Win32::Foundation::BOOL) -> bool {
-        rv.as_bool()
-    }
-
-    pub fn invalid_handle(handle: HANDLE) -> bool {
-        handle.is_invalid()
-    }
-}
-#[cfg(feature = "winapi")]
-mod os {
-    pub use winapi::shared::ntdef::HANDLE;
-    pub use winapi::um::avrt::{AvRevertMmThreadCharacteristics, AvSetMmThreadCharacteristicsA};
-    pub use winapi::um::errhandlingapi::GetLastError;
-
-    pub fn ok(rv: winapi::shared::minwindef::BOOL) -> bool {
-        rv != 0
-    }
-
-    #[allow(non_snake_case)]
-    pub fn PSTR(ptr: *const u8) -> *const i8 {
-        ptr as _
-    }
-
-    pub fn invalid_handle(handle: HANDLE) -> bool {
-        handle.is_null()
-    }
-}
+use windows::Win32::Foundation::GetLastError;
+use windows::Win32::Foundation::HANDLE;
+use windows::Win32::Foundation::PSTR;
+use windows::Win32::System::Threading::{
+    AvRevertMmThreadCharacteristics, AvSetMmThreadCharacteristicsA,
+};
 
 use crate::AudioThreadPriorityError;
 
@@ -42,11 +12,11 @@ use log::info;
 #[derive(Debug)]
 pub struct RtPriorityHandleInternal {
     mmcss_task_index: u32,
-    task_handle: os::HANDLE,
+    task_handle: HANDLE,
 }
 
 impl RtPriorityHandleInternal {
-    pub fn new(mmcss_task_index: u32, task_handle: os::HANDLE) -> RtPriorityHandleInternal {
+    pub fn new(mmcss_task_index: u32, task_handle: HANDLE) -> RtPriorityHandleInternal {
         RtPriorityHandleInternal {
             mmcss_task_index,
             task_handle,
@@ -57,11 +27,11 @@ impl RtPriorityHandleInternal {
 pub fn demote_current_thread_from_real_time_internal(
     rt_priority_handle: RtPriorityHandleInternal,
 ) -> Result<(), AudioThreadPriorityError> {
-    let rv = unsafe { os::AvRevertMmThreadCharacteristics(rt_priority_handle.task_handle) };
-    if !os::ok(rv) {
+    let rv = unsafe { AvRevertMmThreadCharacteristics(rt_priority_handle.task_handle) };
+    if !rv.as_bool() {
         return Err(AudioThreadPriorityError::new(&format!(
             "Unable to restore the thread priority ({:?})",
-            unsafe { os::GetLastError() }
+            unsafe { GetLastError() }
         )));
     }
 
@@ -80,13 +50,13 @@ pub fn promote_current_thread_to_real_time_internal(
     let mut task_index = 0u32;
 
     let handle =
-        unsafe { os::AvSetMmThreadCharacteristicsA(os::PSTR("Audio\0".as_ptr()), &mut task_index) };
+        unsafe { AvSetMmThreadCharacteristicsA(PSTR("Audio\0".as_ptr()), &mut task_index) };
     let handle = RtPriorityHandleInternal::new(task_index, handle);
 
-    if os::invalid_handle(handle.task_handle) {
+    if handle.task_handle.is_invalid() {
         return Err(AudioThreadPriorityError::new(&format!(
             "Unable to restore the thread priority ({:?})",
-            unsafe { os::GetLastError() }
+            unsafe { GetLastError() }
         )));
     }
 
